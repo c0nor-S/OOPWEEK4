5872877ffafc1b9039733a210acfd4cd2aba4369
CONORSHAUGHNESSY-STU <g00432599@atu.ie>
2025-10-06 10:59:50 +0100

    Adding Getters & Setters (NOT FINISHED)

--- src/main/java/ie/atu/week4/Student.java
+++ src/main/java/ie/atu/week4/Student.java
@@ -1,4 +1,24 @@
 package ie.atu.week4;
 
 public class Student {
+    public void setName(String name) {
+    }
+
+    public void setEmail(String email) {
+    }
+
+    public void setCourse(String course) {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCourse() {
+        return course;
+    }
 }
