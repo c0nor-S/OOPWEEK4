2447f51e10bf9227d394e2d1e1c7d1a4d7ac0154
CONORSHAUGHNESSY-STU <g00432599@atu.ie>
2025-10-06 10:19:07 +0100

    Adding Main Info

--- src/main/java/ie/atu/week4/StudentApp.java
+++ src/main/java/ie/atu/week4/StudentApp.java
@@ -5,7 +5,22 @@ import java.util.ArrayList;
 
 public class StudentApp {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
 
+        System.out.println("Please Enter The Number Of Students: ");
+        count = sc.nextInt();
+
+        while (count <=3)
+        {
+            System.out.println("Please Enter Your Name: ");
+            String name = sc.nextLine();
+
+            System.out.println("Please Enter Your Email: ");
+            String email = sc.nextLine();
+
+            System.out.println("Please Enter Your Course: ");
+            String course = sc.nextLine();
+        }
     }
-
 }
